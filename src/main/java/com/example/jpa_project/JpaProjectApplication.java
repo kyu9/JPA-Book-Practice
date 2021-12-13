49af7534cdf68775eff70dec9a1e2377e347de0f
kyu9 <kyudo97@naver.com>
2021-12-13 21:25:36 +0900

    [JPABook] 기본 프로젝트 세팅 중

--- src/main/java/com/example/jpa_project/JpaProjectApplication.java
+++ src/main/java/com/example/jpa_project/JpaProjectApplication.java
@@ -1,9 +1,11 @@
 package com.example.jpa_project;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class JpaProjectApplication {
 
     public static void main(String[] args) {
